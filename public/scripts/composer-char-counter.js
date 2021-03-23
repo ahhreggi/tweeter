ab44ee75c2473649b8142443f55e6cf698181297
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-23 14:20:19 -0400

    Implement counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,14 @@
 $(document).ready(() => {
 
-  $("#tweet-text").on("input", function(event) {
-    console.log(140 - $(this).val().length);
+  $("#tweet-text").on("input", function() {
+    const charsLeft = 140 - $(this).val().length;
+    const counter = $(this).parent().find(".counter");
+    counter.html(charsLeft);
+    if (charsLeft < 0) {
+      counter.addClass("counter-limit");
+    } else {
+      counter.removeClass("counter-limit");
+    }
   });
-
+  
 });
