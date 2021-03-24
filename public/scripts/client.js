319ccf48d0397051915fe7cd8f840859411a36f5
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-24 14:52:04 -0400

    Add convertTimestamp function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -103,14 +103,14 @@ $(document).ready(() => {
 
 
   // Toggle the visibility of the user's @handle on hover
-  $(".tweet").on("mouseenter", function() {
+  $(".tweet").on("mouseenter", function () {
 
     const handle = $(this).find(".handle");
     handle.css("visibility", "visible");
 
   });
 
-  $(".tweet").on("mouseleave", function() {
+  $(".tweet").on("mouseleave", function () {
 
     const handle = $(this).find(".handle");
     handle.css("visibility", "hidden");
