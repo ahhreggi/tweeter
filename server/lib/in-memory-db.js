0d19529865b1bffa3da3027f0042995229ec6f33
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-24 21:00:57 -0400

    Update memory db to use Bob Ross tweets

--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -2,9 +2,10 @@
 
 // Requiring a JSON file automatically parses it and returns the data. These
 // are just example tweets to make it less tedious to style the app initially.
+// const initialTweets = require("../data-files/initial-tweets.json")
+const bobRossTweets = require("../data-files/initial-tweets-bobross.json");
 const db = {
-  tweets: require("../data-files/initial-tweets")
+  tweets: bobRossTweets
 };
 
 module.exports = db;
-
